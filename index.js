bb188a32fb3b4d833afac7c55e4af614cbb15117
Maksim Zarubin <89940028777@ya.ru>
2023-12-12 23:57:03 +0800

    b_2

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const { createUser, login } = require('./controllers/users')
 const { addLesson, addWord } = require('./controllers/lessons')
 const app = express()
 
-mongoose.connect('mongodb://89.111.140.120:27017/dvfustude2');
+mongoose.connect('mongodb://89.111.140.120:27017/leka');
 
 app.listen(PORT, () => {
   console.log('da', PORT)
