443846aa5e5a9e331f0627189c5d542d2356a36a
Maksim Zarubin <89940028777@ya.ru>
2023-12-14 19:14:50 +0800

    CORS_added

--- index.js
+++ index.js
@@ -4,15 +4,16 @@ const {PORT = 3000} = process.env
 const User = require('./models/user')
 const { createUser, login } = require('./controllers/users')
 const { addLesson, addWord } = require('./controllers/lessons')
+const { handleCors } = require('./middlewares/cors')
 const app = express()
 
-mongoose.connect('mongodb://89.111.140.120:27017/leka');
+mongoose.connect('mongodb://localhost:3306/dvfustuden');
 
 app.listen(PORT, () => {
   console.log('da', PORT)
 })
 app.use(express.json());
-
+app.use(handleCors)
 
 app.post('/signup', createUser)
 app.post('/signin', login)
