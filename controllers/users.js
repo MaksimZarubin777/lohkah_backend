acd601387617353c1631d97462e5815324fbc939
Maksim Zarubin <89940028777@ya.ru>
2023-12-18 18:13:51 +0800

    user_cookieparser+

--- controllers/users.js
+++ controllers/users.js
@@ -52,7 +52,7 @@ const login = (req, res, next) => {
 }
  
 const getUserData = (req, res, next) => {
-console.log(req.cookies, 'cookies')
+console.log(req.cookies, 'cookies tut')
 
   // User.findOne({ _id: userId })
   //   .then((user) => {
