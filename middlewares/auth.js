9c64b48262754714cfcc82609941d1e86dad5e03
Maksim Zarubin <89940028777@ya.ru>
2023-12-18 17:32:39 +0800

    user_test

---
+++ middlewares/auth.js
@@ -0,0 +1,21 @@
+const token = require('jsonwebtoken');
+const ConflictError = require('../errors/ConflictError');
+
+module.exports = (req, res, next) => {
+  console.log('auth started')
+  console.log('auth cookies', req.cookies)
+  const { jwt } = req.cookies;
+  if (!jwt) {
+    return next(new ConflictError);
+  }
+  let payload;
+  try {
+    payload = token.verify(jwt, 'dev-secret');
+  } catch (err) {
+    return next(new ConflictError);
+  }
+  req.user = payload;
+  console.log('aauth ending')
+  console.log('aauth req user i payload', req.user, payload)
+  return next();
+};
